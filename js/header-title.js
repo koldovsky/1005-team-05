476eb86e28e5cd7c4dbe8a8ea87a4caae04cc27c
Dmytro Samotii <dimid85@ukr.net>
2023-04-27 14:51:40 +0300

    Modigy index.htl

--- js/header-title.js
+++ js/header-title.js
@@ -1,7 +1,19 @@
-const navbarContent = document.querySelector('#navbarContent');
-const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const navbarTogler = document.querySelector('navbar-colapse');
+const body = document.querySelector('body');
 
-navbarToggler.addEventListener('click', () => {
-    navbarContent.classList.toggle('show');
-    document.body.classList.toggle('modal-open');
+navbarCollapse.addEventListener('show.bs.collapse', function () {
+  body.classList.add('overflow-hidden');
+});
+
+navbarCollapse.addEventListener('hide.bs.collapse', function () {
+    body.classList.remove('overflow-hidden');
+    body.classList.add('auto');
+});
+navbarCollapse.addEventListener('click', () => {
+    if (navbarContent.classList.contains('show')) {
+        body.classList.remove('overflow-hidden');
+        body.classList.add('auto');
+        body.navbarCollapse.remove('collapse show');
+    }
 });
