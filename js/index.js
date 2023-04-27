745e43841d6d59ed0917ee2bed8cac29502b2345
Dmytro Samotii <dimid85@ukr.net>
2023-04-27 23:03:02 +0300

    fix problem opening burger and carousel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("partialsLoaded", () => {
   // тут треба поміняти на ваші JS файли
+  import("./header-icon.js");
   import("./header-title.js");
   import("./index-partners.js");
-  import("./header-icon.js");
+  
 });
