af64d32c07d07e2c716f6204a70e7038ada0dee4
Dmytro Samotii <dimid85@ukr.net>
2023-04-30 12:51:22 +0300

    Fixed bugs

--- js/header-burger.js
+++ js/header-burger.js
@@ -1,19 +1,19 @@
-const navbarCollapse = document.querySelector('.navbar-collapse');
-const navbarTogler = document.querySelector('navbar-colapse');
-const body = document.querySelector('body');
+const navbarCollapse = document.querySelector(".navbar-collapse");
+const body = document.querySelector("body");
+const folding = document.querySelector(".navbar-toggler");
 
-navbarCollapse.addEventListener('show.bs.collapse', function () {
-  body.classList.add('overflow-hidden');
+navbarCollapse.addEventListener("show.bs.collapse", function () {
+  body.classList.add("overflow-hidden");
 });
 
-navbarCollapse.addEventListener('hide.bs.collapse', function () {
-    body.classList.remove('overflow-hidden');
-    body.classList.add('auto');
+navbarCollapse.addEventListener("hide.bs.collapse", function () {
+  body.classList.remove("overflow-hidden");
 });
-navbarCollapse.addEventListener('click', () => {
-    if (navbarContent.classList.contains('show')) {
-        body.classList.remove('overflow-hidden');
-        body.classList.add('auto');
-        
-    }
+navbarCollapse.addEventListener("click", () => {
+  if (navbarCollapse.classList.contains("show")) {
+    body.classList.remove("overflow-hidden");
+    folding.classList.add("collapsed");
+    navbarCollapse.classList.remove("show");
+    folding.setAttribute("aria-expanded", false);
+  }
 });
