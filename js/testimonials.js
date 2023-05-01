09fad07421cd1ed60cd15d689be2d386286531b5
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-05-01 16:20:56 +0300

    create 'api' folder for json. added testimonials.js for my section Testimonials

---
+++ js/testimonials.js
@@ -0,0 +1,27 @@
+
+fetch('api/testimonials.json')
+    .then(response => response.json() )
+    .then(testimonials => renderTestimonials(testimonials));
+
+
+function renderTestimonials(testimonials) {
+    const testimonialsList = document.querySelector('.testimonials__cards');
+    testimonialsList.innerHtml = '';
+    for (const testimonial of testimonials){
+        testimonialsList.innerHTML += createTestimonialHtml(testimonial);
+    }
+}
+
+
+function createTestimonialHtml(testimonial){
+    const newColor = (testimonial.id%2===0 ? 'new_color': '');
+    return  `<article class="testimonials__cards_items ${newColor}">
+                <p class="card_items_text">${testimonial.message}</p>
+                <p class="card_data">${testimonial.massageData}</p>
+                <div class="card_foto">
+                    <img class="card_foto__item" src="${testimonial.clientFoto}" alt="Lucy Peterson foto" class="card_foto_items">
+                </div>
+                <p class="card_name">${testimonial.clientName}</p>
+            </article>`
+}
+
