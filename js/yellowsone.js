dfb743906dd0d87a38c6963721f6079f6cc27810
AlanaSagaidak <109764549+AlanaSagaidak@users.noreply.github.com>
2023-05-03 22:12:49 +0300

    Changed quotes.css

--- js/yellowsone.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("partialsLoaded", () => {
-  import(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-  );
-  import("./yellowstone-quotes");
-});
