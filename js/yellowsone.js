56d3a8768ef7359655d7e298c648850eca53558a
AlanaSagaidak <109764549+AlanaSagaidak@users.noreply.github.com>
2023-05-03 10:19:50 +0300

    Added quotes

---
+++ js/yellowsone.js
@@ -0,0 +1,6 @@
+document.addEventListener("partialsLoaded", () => {
+  import(
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+  );
+  import("./yellowstone-quotes");
+});
